d9545701b0f9ee58eb6514a1c3d13230d73459a9
Pradeep Manandhar <sayamipradeep5@gmail.com>
2025-06-09 21:47:32 +0545

    Updated namespace

--- app/Services/Implementation/AuthService.php
+++ app/Services/Implementation/AuthService.php
@@ -1,5 +1,8 @@
 <?php
 
+namespace App\Services\Implementation;
+use App\Services\Interface\AuthServiceInterface;
+
 class AuthService implements AuthServiceInterface
 {
   public function checkLogin()
--- app/Services/Interface/AuthServiceInterface.php
+++ app/Services/Interface/AuthServiceInterface.php
@@ -1,5 +1,5 @@
 <?php
-
+ namespace App\Services\Interface;
 
 interface AuthServiceInterface
 {
